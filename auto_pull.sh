0f53398034a571c1ff15bfc9cb98699e01e988b7
TencentCloud <1105770801@qq.com>
2023-10-20 08:56:59 +0800

    update the auto_pull.sh

---
+++ auto_pull.sh
@@ -0,0 +1,5 @@
+cd /home/ubuntu/MyCodeProject/EE_Django
+
+# 执行git pull命令，并将输出重定向到一个日志文件
+echo "---------- Pulling on $(date) ----------" >> /home/ubuntu/MyCodeProject/EE_Django/pull_log.txt
+git pull >> /home/ubuntu/MyCodeProject/EE_Django/pull_log.txt 2>&1
